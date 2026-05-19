836916d808c61c448a09ac15b6ca82a1b992ea94
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-19 18:11:50 -0500

    added constructors and getters to bankaccount

--- src/main/java/com/pluralsight/finance/BankAccount.java
+++ src/main/java/com/pluralsight/finance/BankAccount.java
@@ -6,7 +6,30 @@ public class BankAccount implements IValuable{
     private String accountNumber;
     private double balance;
 
+// add constructor
 
+    public BankAccount(String name, String accountNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    //add getters
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+    //add derived variables
 
 
 }

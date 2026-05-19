93ef216dd1630786e22713bb7c7d27b9b15e1c88
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-19 18:33:09 -0500

    added getters and constructor and plan for derived method

--- src/main/java/com/pluralsight/finance/CreditCard.java
+++ src/main/java/com/pluralsight/finance/CreditCard.java
@@ -9,8 +9,33 @@ public class CreditCard implements IValuable {
 
     // add constructor
 
+    public CreditCard(String name, String accountNumber, double balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+
     //add getter
 
+    public String getName() {
+        return name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
     //add value
+    @Override
+    public double getValue() {
+        return 0;
+    }
+
+    //add derived methods
 
 }

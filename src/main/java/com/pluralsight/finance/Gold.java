0bd7b206179b0d25c7cf22986e4a85eab9cc8f10
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-19 22:00:40 -0500

    -add weight

--- src/main/java/com/pluralsight/finance/Gold.java
+++ src/main/java/com/pluralsight/finance/Gold.java
@@ -2,7 +2,13 @@ package com.pluralsight.finance;
 
 public class Gold extends FixedAsset{
 
+    // variables for weight
      private double weight;
+
+     //add constructors
+
+
+    //add getters
 
 }
 

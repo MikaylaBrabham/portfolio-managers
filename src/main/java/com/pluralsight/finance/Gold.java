1ba940d7478f3e2e3abf8ac4698d4af276075eba
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-19 19:50:28 -0500

    added market value and value

--- src/main/java/com/pluralsight/finance/Gold.java
+++ src/main/java/com/pluralsight/finance/Gold.java
@@ -3,8 +3,26 @@ package com.pluralsight.finance;
 public class Gold extends FixedAsset {
 
     //add constructor from fixed asset
-
     public Gold(String name, double marketValue, double value) {
         super(name, marketValue, value);
+
+    }
+    //add marketvalue and override as karat
+    public double karat(double marketValue){
+        return marketValue;
+    }
+
+    // add jewelry
+    public void jewelry(){
+
+    }
+
+
+
+    // add value
+    @Override
+    public double getValue() {
+        return super.getValue();
     }
 }
+

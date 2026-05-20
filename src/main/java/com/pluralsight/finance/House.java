05b3121892837a3c69d15f77d5193ba090c7e2d4
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-19 22:12:45 -0500

    -add override

--- src/main/java/com/pluralsight/finance/House.java
+++ src/main/java/com/pluralsight/finance/House.java
@@ -1,6 +1,6 @@
 package com.pluralsight.finance;
 
-public class House {
+public class House extends FixedAsset{
 
     //add variables
     private int yearBuilt;
@@ -8,5 +8,15 @@ public class House {
     private int bedrooms;
 
     // add constructors
-
+    public House(String name, double marketValue, int yearBuilt, int squareFeet, int bedrooms) {
+        super(name, marketValue);
+        this.yearBuilt = yearBuilt;
+        this.squareFeet = squareFeet;
+        this.bedrooms = bedrooms;
+    }
+    // get value
+    @Override
+    public double getValue() {
+        return super.getValue();
+    }
 }

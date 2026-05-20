0d6d75c19baa6281dd3d9b2e0fbf492c23b09836
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-19 23:39:17 -0500

    add accounts example

--- src/main/java/com/pluralsight/FinanceApplication.java
+++ src/main/java/com/pluralsight/FinanceApplication.java
@@ -1,18 +1,30 @@
 package com.pluralsight;
 
-import com.pluralsight.finance.BankAccount;
-import com.pluralsight.finance.IValuable;
+import com.pluralsight.finance.*;
 
 
-
-public class FinanceApplication  {
+public class FinanceApplication {
     public static void main(String[] args) {
         BankAccount account1 = new BankAccount(123, "Pam", 12500);
         IValuable account2 = new BankAccount(456, "Gary", 1500);
 // try to deposit money into both accounts
         account1.deposit(100);
         //account2.deposit(100);
-    }
+        //account 2 doesn't work because we didn't declare it in the valuable the only rule passed is the need to get value
 
-    //account 2 doesn't work because we didn't declare it in the valuable the only rule passed is theneed to get value
+
+
+        //updated to add  bonus
+        Portfolio myPortfolio = new Portfolio("My Finances", "Mikayla <3");
+
+        // add accounts
+        BankAccount ChaseBankChecking = new BankAccount(98565097, "Danese", 1038.87);
+        BankAccount ChaseBankSaving = new BankAccount(12345623, "Mikayla", 876.13);
+        CreditCard Discover = new CreditCard("Lola", 8763682, 600 );
+
+        // add
+
+
+
+    }
 }
